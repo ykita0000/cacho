a00f179ec681d94db160fc03b4c60b601a1567b6
ykita0000 <ykita0000@gmail.com>
2015-12-11 16:08:57 +0900

    add movie

---
+++ shootMovie.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+#coding:utf-8
+'''
+name   : .py
+author : ykita
+date   : 
+memo   :  
+'''
+import os, os.path
+import sys
+import time
+
+### loop one ###
+def shootMovie():
+    sys.stdout.write("          @\n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    time.sleep(0.1)
+    sys.stdout.write("\033[5A")
+    
+    sys.stdout.write("           \n")
+    sys.stdout.write("        @  \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    time.sleep(0.1)
+    sys.stdout.write("\033[5A")
+    
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("      @    \n")
+    time.sleep(0.1)
+    sys.stdout.write("\033[5A")
+    
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("     @     \n")
+    sys.stdout.write("           \n")
+    time.sleep(0.1)
+    sys.stdout.write("\033[5A")
+    
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("    @      \n")
+    time.sleep(0.1)
+    sys.stdout.write("\033[5A")
+    
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("           \n")
+    sys.stdout.write("   @       \n")
+    time.sleep(0.4)
+    sys.stdout.write("\033[5A")
